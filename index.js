ba1316d4803b1060d40d5f174cb9bebe72dadb57
Balint Soos <soosbalint95@gmail.com>
2024-09-19 17:49:16 +0200

    add async versions for add user and auth endpoints

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ router.get('/users', (context) => {
   context.body = users;
 });
 
-router.post('/users', (context) => {
+router.post('/users/sync', (context) => {
   let username = context.request.body.username || '';
   const password = context.request.body.password || '';
 
@@ -30,11 +30,35 @@ router.post('/users', (context) => {
   const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512');
 
   users[username] = { salt, hash };
-
   context.status = 201;
 });
 
-router.post('/auth', (context) => {
+router.post('/users/async', (context) => {
+  let username = context.request.body.username || '';
+  const password = context.request.body.password || '';
+
+  username = username.replace(/[!@#$%^&*]/g, '');
+
+  if (!username || !password || users[username]) {
+    context.status = 400;
+    return;
+  }
+
+  const salt = crypto.randomBytes(128).toString('base64');
+  crypto.pbkdf2(
+    password,
+    salt,
+    10000,
+    512,
+    'sha512',
+    (err, hash) => {
+      users[username] = { salt, hash };
+      context.status = 201;
+    }
+  );
+});
+
+router.post('/auth/sync', (context) => {
   let username = context.request.body.username || '';
   const password = context.request.body.password || '';
 
@@ -53,6 +77,33 @@ router.post('/auth', (context) => {
   } else {
     context.status = 401;
   }
+});
+
+router.post('/auth/async', (context) => {
+  let username = context.request.body.username || '';
+  const password = context.request.body.password || '';
+
+  username = username.replace(/[!@#$%^&*]/g, '');
+
+  if (!username || !password || !users[username]) {
+    context.status = 400;
+    return;
+  }
+
+  crypto.pbkdf2(
+    password,
+    users[username].salt,
+    10000,
+    512,
+    'sha512',
+    (err, hash) => {
+      if (users[username].hash.toString() === hash.toString()) {
+        context.status = 200;
+      } else {
+        context.status = 401;
+      }
+    }
+  );
 });
 
 const app = new Koa();
